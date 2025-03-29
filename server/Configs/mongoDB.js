d89bca6740add7a2de51dda414637f4519e0eff2
Rajiv-Sharma23 <rajivsharma0246@gmail.com>
2025-03-29 16:20:21 +0530

    c

--- server/Configs/mongoDB.js
+++ server/Configs/mongoDB.js
@@ -5,10 +5,7 @@ export const connectDB = async () => {
         mongoose.connection.on("connected", () => {
             console.log("Database connected");
         })
-        const conect = await mongoose.connect(`${process.env.MONGODB_URI}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          });
+        const conect = await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`MongoDB connected successfully- ${conect.connection.host}`);
     } catch (error) {
         console.log(error);
